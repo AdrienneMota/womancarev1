cf1ba83723fd0c70ce28a91dd4a6edd75bd47307
AdrienneMota <adriennemota97@gmail.com>
2023-04-02 21:50:05 -0300

    feat: rendering the payment options.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import RequestedDetails from "./pages/RequestedDetails";
-import { Payment } from "./pages/Payment";
+import Payment from "./pages/Payment";
+import Card from "./pages/Card";
+import Pix from "./pages/Pix";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/details/:requestId" element={<RequestedDetails/>}/>
         <Route path="/details/payment" element={<Payment/>}/>
+        <Route path="/card" element={<Card/>}/>
+        <Route path="/pix" element={<Pix/>}/>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,3 +1,41 @@
-export function Payment() {
-    return <>Realize o pagamento em pix ou cartão</>
+import { useState } from "react"
+
+export default function Payment() {
+    const [methodPayment, setmethodPayment] = useState("")
+    
+    return (
+        <>
+           <h1>Método de pagamento</h1>
+               <div>
+                  <input 
+                    type="radio" 
+                    name="option_payment" 
+                    value="card" 
+                    onChange={(e) => {setmethodPayment(e.target.value)}}
+                  />
+                  <label htmlFor="card">cartão</label>
+              </div>
+              <div>
+                  <input 
+                    type="radio" 
+                    name="option_payment" 
+                    value="pix"
+                    onChange={(e) =>  {setmethodPayment(e.target.value)}}
+                    />
+                  <label htmlFor="pix">Pix</label>
+               </div>   
+            {
+                (methodPayment==="null")? 
+                   ""
+                   :
+                   (methodPayment==="pix")?
+                   <div>pix</div>
+                   :
+                   (methodPayment==="card")?
+                   <div>card</div>
+                   :
+                   ""
+            }
+        </>
+    )
 }
