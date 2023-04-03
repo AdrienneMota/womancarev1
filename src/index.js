0bbd4ac83299fcd40813e3403553ce9054a98514
AdrienneMota <adriennemota97@gmail.com>
2023-04-02 21:52:48 -0300

    fix: bug in rendering pix qr code.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Buffer } from 'buffer'
+// @ts-expect-error
+window.Buffer = window.Buffer ?? Buffer
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
